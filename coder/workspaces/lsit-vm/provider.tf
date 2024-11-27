76ca3243c282bee823c8ecb10e307f87270508da
Seth Erickson <sr.erickson@gmail.com>
2024-11-27 15:00:19 -0800

    wip

--- coder/workspaces/lsit-vm/provider.tf
+++ coder/workspaces/lsit-vm/provider.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
+    coder = {
+      source = "coder/coder"
+    }
     harvester = {
       source  = "harvester/harvester"
       version = "0.6.5"
