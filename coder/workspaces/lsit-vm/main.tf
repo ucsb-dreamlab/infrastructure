f568bf06e976b65088aaa8614320799d4b4f100c
Seth Erickson <sr.erickson@gmail.com>
2025-01-27 12:15:35 -0800

    updates to LIST Code workspace (#10)

--- coder/workspaces/lsit-vm/main.tf
+++ coder/workspaces/lsit-vm/main.tf
@@ -1,5 +1,5 @@
 data "harvester_image" "os_image" {
-  display_name = "almalinux-9-genericcloud-9.5-20241120.x86_64"
+  display_name = "Ubuntu Minimal 24.04 LTS"
   namespace = var.namespace
 }
 
@@ -9,7 +9,26 @@ locals {
   # names used for coder's resources in harvester: needs to be valid for
   # kubernetes IDs.
   k8s_name = lower(replace("coder-${data.coder_workspace_owner.me.name}-${data.coder_workspace.env.name}"," ","_"))
+  workspace_agreement = "https://coder.dreamlab.ucsb.edu/templates/lsit-vm/docs"
 }
+
+
+data "coder_parameter" "agreement" {
+  name = "user_agreement"
+  display_name = "User agreement"
+  type = "string"
+  mutable = true
+  description = <<-EOT
+    Please confirm that you have read and understand the [usage policies for the workspace](${local.workspace_agreement}).
+    EOT
+
+  option {
+    name = "I understand the usage policies."
+    value = "agree"
+  }
+
+}
+
 
 data "coder_workspace" "env" {}
 data "coder_workspace_owner" "me" {}
@@ -25,6 +44,13 @@ resource "coder_agent" "dev" {
     curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
     /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
   EOT
+
+  display_apps {
+    vscode          = true
+    vscode_insiders = false
+    web_terminal    = true
+    ssh_helper      = false
+  }
 
   metadata {
     key          = "cpu"
@@ -49,20 +75,40 @@ resource "coder_agent" "dev" {
   }
 }
 
-resource "coder_app" "code-server" {
+resource "coder_app" "rstudio" {
   count        = data.coder_workspace.env.start_count
   agent_id     = coder_agent.dev[0].id
-  slug         = "code-server"
-  display_name = "code-server"
-  url          = "http://localhost:13337/?folder=/home/coder"
-  icon         = "/icon/code.svg"
-  subdomain    = false
+  slug         = "rstudio"
+  display_name = "RStudio"
+  url          = "http://localhost:8787"
+  icon         = "/icon/rstudio.svg"
+  subdomain    = true
   share        = "owner"
+  order        = 1
   healthcheck {
-    url       = "http://localhost:13337/healthz"
-    interval  = 3
-    threshold = 10
+    url       = "http://localhost:8787/health-check"
+    interval  = 4
+    threshold = 40
   }
+}
+
+module "filebrowser" {
+  count    = data.coder_workspace.env.start_count
+  source   = "registry.coder.com/modules/filebrowser/coder"
+  version  = "1.0.23"
+  order        = 2
+  agent_id = coder_agent.dev[0].id
+  database_path = ".config/filebrowser.db"
+}
+
+module "vscode-web" {
+  count = data.coder_workspace.env.start_count
+  source = "registry.coder.com/modules/vscode-web/coder"
+  version = "1.0.26"
+  order = 3
+  agent_id = coder_agent.dev[0].id
+  accept_license = true
+  folder = "/home/coder"
 }
 
 # harvester requires an ssh key in the vm's userdata. This isn't actually used.
@@ -87,11 +133,20 @@ resource "harvester_cloudinit_secret" "coder-userdata" {
   })
 }
 
+# operating system disk
 resource "harvester_volume" "coder-disk" {
   name = local.k8s_name
   namespace = var.namespace
   image = data.harvester_image.os_image.id
-  size = "60Gi"
+  size = "20Gi"
+}
+
+# user data disk
+resource "harvester_volume" "user-disk" {
+  name = "${local.k8s_name}-userdata"
+  namespace = var.namespace
+  storage_class_name = "csi-rbd-sc"
+  size = "100Gi"
 }
 
 resource "harvester_virtualmachine" "coder-vm" {
@@ -99,7 +154,7 @@ resource "harvester_virtualmachine" "coder-vm" {
     namespace = var.namespace
     description = "coder vm: ${data.coder_workspace_owner.me.name} ${data.coder_workspace.env.name}"
     run_strategy = data.coder_workspace.env.transition == "start" ? "RerunOnFailure" : "Halted"
-    cpu = 8
+    cpu = 4
     memory = "16Gi"
     disk {
         name       = "rootdisk"
@@ -109,12 +164,19 @@ resource "harvester_virtualmachine" "coder-vm" {
         existing_volume_name = harvester_volume.coder-disk.name
         auto_delete          = false
     }
+    disk {
+        name       = "user-disk"
+        type       = "disk"
+        bus        = "virtio"
+        existing_volume_name = harvester_volume.user-disk.name
+        auto_delete = false
+    }
     network_interface {
+        # management network is default
         name         = "default"
         model        = "virtio"
-        type         = "bridge"
-        # using public network as it's faster
-        network_name = "default/2176"
+        network_name = "harvester-public/1173"
+        type = "bridge"        
     }
     ssh_keys = [harvester_ssh_key.key.id]
     cloudinit {
